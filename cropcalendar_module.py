a58c6d7e148db743ba3da1e4c3ab706bbebcccee
Seyed Hossein Hosseini <114243033+sehohosseini@users.noreply.github.com>
2025-09-19 15:56:04 +0300

    Update and rename cropcalendar.py to cropcalendar_module.py

--- cropcalendar_module.py
+++ cropcalendar_module.py
@@ -8,28 +8,33 @@ def cropcalendar(domain_path, basepath, referenceraster_path):
     from shapely.geometry import mapping
     from rasterio.warp import Resampling
     from preproc_tools import download_url, unzip_all, ensure_xy_dims, makedirs
-
-    # THIS SCRIPT COULD BE EXTENDED BY MANUAL DELAY OF PLANTING DATE USING RAINFALL THRESHOLDS (min. 10 mm rainfall/month), TO AVOID DYING OF CROPS WHEN PLANTED TOO EARLY (HAPPENS A LOT IN THE MEKONG WHEN USING DEFAULT CROP CALENDAR)
+    from xarray.coding.times import CFTimedeltaCoder
 
     # For reprojecting and clipping
     mask = gpd.read_file(domain_path)
     to_match = xr.open_dataset(referenceraster_path)
     to_match.rio.write_crs(4326, inplace=True)
 
-
     ## Download crop calendar dataset from GGCMI (Jägermeyr et al. 2021; https://zenodo.org/records/5062513)
     url = 'https://zenodo.org/api/records/5062513/files-archive'
-
     target_dir = makedirs(basepath, 'rawdata', 'cropcalendar')
     download_path = os.path.join(target_dir, 'ggcmi_cropcalendar.zip')
+    unzipped_download_directory = download_path[:-4]  # assumes unzip goes to same-named folder
 
-    print("        *** DOWNLOADING GGCMI crop calendar data ***")
-    print('        URL: ', url)
-    download_url(url, download_path=download_path)
+    #  Skip everything if already unzipped
+    if os.path.exists(unzipped_download_directory):
+        print(f" GGCMI crop calendar already unzipped, skipping: {unzipped_download_directory}")
+    else:
+        #  Skip download if ZIP exists
+        if os.path.exists(download_path):
+            print(f" ZIP already exists, skipping download: {download_path}")
+        else:
+            print(" Downloading GGCMI crop calendar data")
+            print('   URL:', url)
+            download_url(url, download_path=download_path)
 
-    print("        *** UNZIPPING GGCMI crop calendar data ***")
-    unzip_all(dir=target_dir)
-    unzipped_download_directory = download_path[:-4]
+        print("Unzipping GGCMI crop calendar data...")
+        unzip_all(dir=target_dir)
 
 
     ## Convert separate files to rasterstack, reproject (nearest neighbour) to 0.05 degrees, and clip to domain
@@ -49,7 +54,7 @@ def cropcalendar(domain_path, basepath, referenceraster_path):
             os.remove(layer)
             continue
 
-        src = xr.open_dataset(layer)
+        src = xr.open_dataset(layer, decode_timedelta=False)       
         src = ensure_xy_dims(src)
         src = src.drop_vars(['growing_season_length', 'data_source_used'])
         src.rio.write_crs(4326, inplace=True)
